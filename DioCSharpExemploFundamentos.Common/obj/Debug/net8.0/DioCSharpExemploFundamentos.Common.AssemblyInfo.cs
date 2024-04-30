44538198fe9d16d404c355b0fa80f9d5abfc5035
LucasCordeiro-dev <lukacordeiro@hotmail.com>
2024-04-30 20:50:33 -0300

    Add novo csproj e refatorando codigo

--- DioCSharpExemploFundamentos.Common/obj/Debug/net8.0/DioCSharpExemploFundamentos.Common.AssemblyInfo.cs
+++ DioCSharpExemploFundamentos.Common/obj/Debug/net8.0/DioCSharpExemploFundamentos.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DioCSharpExemploFundamentos.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79edb08dd1452d478b47a80281d29122c47f7a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("DioCSharpExemploFundamentos.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DioCSharpExemploFundamentos.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
